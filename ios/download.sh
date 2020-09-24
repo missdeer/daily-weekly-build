4ea1198425f1bdbb0223f54396f783c1acce9de3
Fan Yang <fyang3@cisco.com>
2020-09-24 11:42:03 +0800

    (*)fix download save name conflict

--- ios/download.sh
+++ ios/download.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -e
+#!/bin/bash -e
 
 # Change to preferred versions
 MPV_VERSION="0.32.0"
@@ -9,20 +9,28 @@ HARFBUZZ_VERSION="2.6.7"
 FRIBIDI_VERSION="1.0.10"
 UCHARDET_VERSION="0.0.7"
 
+rm -rf src
+mkdir src
+[ -d "downloads" ] || mkdir downloads;
+
 MPV_URL="https://github.com/mpv-player/mpv/archive/master.tar.gz"
+curl -f -L $MPV_URL -o downloads/mpv.tar.gz
+tar xvf downloads/mpv.tar.gz -C src
+
 FFMPEG_URL="https://github.com/FFmpeg/FFmpeg/archive/master.tar.gz"
+curl -f -L $FFMPEG_URL -o downloads/FFmpeg.tar.gz
+tar xvf downloads/FFmpeg.tar.gz -C src
+
 LIBASS_URL="https://github.com/libass/libass/archive/master.tar.gz"
 FRIBIDI_URL="https://github.com/fribidi/fribidi/releases/download/v$FRIBIDI_VERSION/fribidi-$FRIBIDI_VERSION.tar.xz"
 FREETYPE_URL="https://sourceforge.net/projects/freetype/files/freetype2/$FREETYPE_VERSION/freetype-$FREETYPE_VERSION.tar.xz"
 HARFBUZZ_URL="https://www.freedesktop.org/software/harfbuzz/release/harfbuzz-$HARFBUZZ_VERSION.tar.xz"
 UCHARDET_URL="https://www.freedesktop.org/software/uchardet/releases/uchardet-$UCHARDET_VERSION.tar.xz"
 
-rm -rf src
-mkdir -p src downloads
-for URL in $UCHARDET_URL $FREETYPE_URL $HARFBUZZ_URL $FRIBIDI_URL $LIBASS_URL $FFMPEG_URL $MPV_URL; do
+for URL in $UCHARDET_URL $FREETYPE_URL $HARFBUZZ_URL $FRIBIDI_URL $LIBASS_URL; do
 	TARNAME=${URL##*/}
     if [ ! -f "downloads/$TARNAME" ]; then
-	    curl -f -L -- $URL > downloads/$TARNAME
+	    curl -f -L $URL -o downloads/$TARNAME
     fi
     echo "$TARNAME"
     tar xvf downloads/$TARNAME -C src
