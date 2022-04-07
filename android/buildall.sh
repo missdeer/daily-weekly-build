68d0742bea493fd3db0159e4674bcd7d6525a37f
Fan Yang <fanyang@cadg.cn>
2022-04-07 09:46:46 +0800

    sync with upstream

--- android/buildall.sh
+++ android/buildall.sh
@@ -17,7 +17,7 @@ getdeps () {
 loadarch () {
 	unset CC CXX CPATH LIBRARY_PATH C_INCLUDE_PATH CPLUS_INCLUDE_PATH
 
-	apilvl=21
+	local apilvl=21
 	# ndk_triple: what the toolchain actually is
 	# cc_triple: what Google pretends the toolchain is
 	if [ "$1" == "armv7l" ]; then
@@ -62,6 +62,9 @@ setup_prefix () {
 		ln -s . "$prefix_dir/local"
 	fi
 
+	local cpu_family=${ndk_triple%%-*}
+	[ "$cpu_family" == "i686" ] && cpu_family=x86
+
 	# meson wants to be spoonfed this file, so create it ahead of time
 	# also define: release build, static libs and no source downloads at runtime(!!!)
 	cat >"$prefix_dir/crossfile.txt" <<CROSSFILE
@@ -77,7 +80,7 @@ strip = '$ndk_triple-strip'
 pkgconfig = 'pkg-config'
 [host_machine]
 system = 'android'
-cpu_family = '${ndk_triple%%-*}'
+cpu_family = '$cpu_family'
 cpu = '${CC%%-*}'
 endian = 'little'
 CROSSFILE
@@ -90,7 +93,7 @@ build () {
 	fi
 	echo >&2 -e "\033[1;34mBuilding $1...\033[m"
 	if [ $nodeps -eq 0 ]; then
-		deps=$(getdeps $1)
+		local deps=$(getdeps $1)
 		echo >&2 "Dependencies: $deps"
 		for dep in $deps; do
 			build $dep
@@ -111,9 +114,9 @@ build () {
 usage () {
 	echo "Usage: buildall.sh [options] [target]"
 	echo "Builds the specified target (default: $target)"
+	echo "-n             Do not build dependencies"
 	echo "--clean        Clean build dirs before compiling"
-	echo "--no-deps      Do not build dependencies"
-	echo "--gcc          Use gcc compiler (not officially supported!)"
+	echo "--gcc          Use gcc compiler (unsupported!)"
 	echo "--arch <arch>  Build for specified architecture (default: $arch; supported: armv7l, arm64, x86_64)"
 	exit 0
 }
@@ -123,7 +126,7 @@ while [ $# -gt 0 ]; do
 		--clean)
 		cleanbuild=1
 		;;
-		--no-deps)
+		-n|--no-deps)
 		nodeps=1
 		;;
 		--gcc)
@@ -148,6 +151,6 @@ setup_prefix
 build $target
 
 [ "$target" == "mpv-android" ] && \
-	ls -lh ../app/build/outputs/apk/{debug,release}/*.apk
+	ls -lh ../app/build/outputs/apk/{default,api29}/*/*.apk
 
 exit 0
